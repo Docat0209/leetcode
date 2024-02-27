c0dd76c26271313d23f45da13b9d1bef67ea1df4
Docat <89736275+Docat0209@users.noreply.github.com>
2024-02-27 22:51:29 +0800

    docs: fix add2Num date
    change add2Num date to 02-27-2024
    
    LeetCode #2

--- src/add2Num/add2Num.cpp
+++ src/add2Num/add2Num.cpp
@@ -1,6 +1,6 @@
 // Source : https://leetcode.com/problems/add-two-numbers/
 // Author : Docat
-// Date   : 02-26-2024
+// Date   : 02-27-2024
 
 /*
 You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
